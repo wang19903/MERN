ff7f54d4363d6237671a016563400f99320b6bd0
wangg <lf21715@yahoo.com.tw>
2022-10-08 17:53:40 +0800

    d

--- server/routes/course-route.js
+++ server/routes/course-route.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 const router = require("express").Router();
 const Course = require("../models").courseModel;
 const courseValidation = require("../vaildation").courseValidation;
@@ -5,6 +7,32 @@ const courseValidation = require("../vaildation").courseValidation;
 router.use((req, res, next) => {
   console.log("A request is coming into api...");
   next();
+});
+
+router.get("/", (req, res) => {
+  Course.find({})
+    //populate將ref綁定的資料一起帶出，沒+populate只會有導師ID，加入後出現老師[姓名、信箱]
+    .populate("instructor", ["username", "email"])
+    .then((course) => {
+      res.send(course);
+    })
+    .catch((err) => {
+      console.log("get course: " + err);
+      res.status(500).send("cant get course");
+    });
+});
+
+router.get("/:_id", (req, res) => {
+  let { _id } = req.params;
+  Course.findOne({ _id })
+    .populate("instructor", ["username", "email"])
+    .then((course) => {
+      res.send(course);
+    })
+    .catch((err) => {
+      console.log("get a course: " + err);
+      res.status(500).send("cant get a course");
+    });
 });
 
 router.post("/", async (req, res) => {
@@ -23,7 +51,8 @@ router.post("/", async (req, res) => {
   let newCourse = new Course({
     title,
     description,
-    price
+    price,
+    instructor: req.user._id
   });
 
   try {
@@ -31,6 +60,74 @@ router.post("/", async (req, res) => {
     res.status(200).send("new course has been saved.");
   } catch (err) {
     res.status(400).send("cannot save course.");
+  }
+});
+
+router.patch("/:_id", async (req, res) => {
+  const { error } = courseValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let { _id } = req.params;
+  let course = await Course.findOne({ _id });
+  if (!course) {
+    res.status(400);
+    return res.json({
+      succsee: false,
+      message: "course not found."
+    });
+  }
+
+  if (course.instructor.equals(req.user._id) || req.user.isAdmin()) {
+    Course.findOneAndUpdate({ _id }, req.body, {
+      new: true,
+      runValidators: true
+    })
+      .then(() => {
+        res.send("Course updated.");
+      })
+      .catch((err) => {
+        res.json({
+          success: false,
+          message: err
+        });
+      });
+  } else {
+    res.status(403);
+    return res.json({
+      succsee: false,
+      message: "only instructor or admin can edit this course."
+    });
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+  let { _id } = req.params;
+  let course = await Course.findOne({ _id });
+  if (!course) {
+    res.status(400);
+    return res.json({
+      succsee: false,
+      message: "course not found."
+    });
+  }
+
+  if (course.instructor.equals(req.user._id) || req.user.isAdmin()) {
+    Course.deleteOne({ _id })
+      .then(() => {
+        res.send("Course deleted.");
+      })
+      .catch((err) => {
+        res.json({
+          success: false,
+          message: err
+        });
+      });
+  } else {
+    res.status(403);
+    return res.json({
+      succsee: false,
+      message: "only instructor or admin can delete this course."
+    });
   }
 });
 
